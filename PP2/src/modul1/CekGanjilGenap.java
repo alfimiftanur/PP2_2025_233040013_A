a6a65a89f60974bc6732c7220a9a18e5267d84c9
alfimiftanur <alfinurhakim1234@gmail.com>
2025-11-05 15:52:08 +0700

    upload modul 1

---
+++ PP2/src/modul1/CekGanjilGenap.java
@@ -0,0 +1,12 @@
+package modul1;
+
+public class CekGanjilGenap {
+    public static void main(String[] args) {
+        int angka = 7;
+        if (angka %2 == 0){
+            System.out.println("Angka " + angka + " adalah genap");
+        }else {
+            System.out.println("Angka "+ angka + " adalah ganjil");
+        }
+    }
+}
---
+++ PP2/src/modul1/HitungLuas.java
@@ -0,0 +1,11 @@
+package modul1;
+
+public class HitungLuas {
+    public static void main(String[] args) {
+        int panjang = 10;
+        int lebar = 5;
+        int luas = panjang * lebar;
+
+        System.out.println("Luas persegi panjang adalah: "+ luas);
+    }
+}
